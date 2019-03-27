323f8175ad84ac71dbe084c2521de4f44a012f46
ppl <ppl1121@hotmail.com>
2019-03-27 18:44:20 +0800

    Updates

---
+++ precache-manifest.596efc80bc138b2b942f25e005141fa2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "2995d79a12f2b95cd2315d66c5bb5084",
+    "url": "/shopping-cart-ReactAndRedux/static/media/12064273040195392_1.2995d79a.jpg"
+  },
+  {
+    "revision": "c427289a47466c749373",
+    "url": "/shopping-cart-ReactAndRedux/static/css/main.7559ebab.chunk.css"
+  },
+  {
+    "revision": "b36fcaa4762d0a1de0fc",
+    "url": "/shopping-cart-ReactAndRedux/static/js/runtime~main.d723645f.js"
+  },
+  {
+    "revision": "2d0812a42e7fe528a0e4",
+    "url": "/shopping-cart-ReactAndRedux/static/js/2.dfb98c0c.chunk.js"
+  },
+  {
+    "revision": "b5714b6acfbff7904a24ea78662b9345",
+    "url": "/shopping-cart-ReactAndRedux/static/media/100_1.b5714b6a.jpg"
+  },
+  {
+    "revision": "b618b5fc19f0517b8428bc9cae806df0",
+    "url": "/shopping-cart-ReactAndRedux/static/media/101_1.b618b5fc.jpg"
+  },
+  {
+    "revision": "2e62289b2a88630ff3461c81af1756f7",
+    "url": "/shopping-cart-ReactAndRedux/static/media/10412368723880252_1.2e62289b.jpg"
+  },
+  {
+    "revision": "54fbf816a3acf53727a2536d97d76881",
+    "url": "/shopping-cart-ReactAndRedux/static/media/10547961582846888_1.54fbf816.jpg"
+  },
+  {
+    "revision": "0d941b4cc1da99273f35b2777b500ff3",
+    "url": "/shopping-cart-ReactAndRedux/static/media/10686354557628304_1.0d941b4c.jpg"
+  },
+  {
+    "revision": "84c82a5a8f51a94e64024f67e8445d37",
+    "url": "/shopping-cart-ReactAndRedux/static/media/11033926921508488_1.84c82a5a.jpg"
+  },
+  {
+    "revision": "06ec2c628b4a51a539881f0ee02b4e60",
+    "url": "/shopping-cart-ReactAndRedux/static/media/11600983276356164_1.06ec2c62.jpg"
+  },
+  {
+    "revision": "7faf6a31b8557166976235d3bd9a8e2b",
+    "url": "/shopping-cart-ReactAndRedux/static/media/11854078013954528_1.7faf6a31.jpg"
+  },
+  {
+    "revision": "c427289a47466c749373",
+    "url": "/shopping-cart-ReactAndRedux/static/js/main.c5645f4a.chunk.js"
+  },
+  {
+    "revision": "d78e27906d034c3b4cd53aa3526acda3",
+    "url": "/shopping-cart-ReactAndRedux/static/media/18532669286405344_1.d78e2790.jpg"
+  },
+  {
+    "revision": "75f51cecfca671162c53bdeed3cf549f",
+    "url": "/shopping-cart-ReactAndRedux/static/media/18644119330491310_1.75f51cec.jpg"
+  },
+  {
+    "revision": "4c799bac69deb57ceba00bcdf15e6ec2",
+    "url": "/shopping-cart-ReactAndRedux/static/media/27250082398145996_1.4c799bac.jpg"
+  },
+  {
+    "revision": "a313534ce6b430f29143cafee1eb5a3e",
+    "url": "/shopping-cart-ReactAndRedux/static/media/39876704341265610_1.a313534c.jpg"
+  },
+  {
+    "revision": "df947f142806310944c26d76bf1b8fa0",
+    "url": "/shopping-cart-ReactAndRedux/static/media/51498472915966370_1.df947f14.jpg"
+  },
+  {
+    "revision": "843a471381aada57cdd38f66662ab1af",
+    "url": "/shopping-cart-ReactAndRedux/static/media/5619496040738316_1.843a4713.jpg"
+  },
+  {
+    "revision": "45a051e062934c657ca67819884cbf23",
+    "url": "/shopping-cart-ReactAndRedux/static/media/6090484789343891_1.45a051e0.jpg"
+  },
+  {
+    "revision": "87b5353e208124cb9acc7cadd973a3bb",
+    "url": "/shopping-cart-ReactAndRedux/static/media/8552515751438644_1.87b5353e.jpg"
+  },
+  {
+    "revision": "df97d7c2d0fc5e5d9346b660ce3936fa",
+    "url": "/shopping-cart-ReactAndRedux/static/media/876661122392077_1.df97d7c2.jpg"
+  },
+  {
+    "revision": "59b643658d74a090ed326573a46e07a7",
+    "url": "/shopping-cart-ReactAndRedux/static/media/9197907543445676_1.59b64365.jpg"
+  },
+  {
+    "revision": "9b698d6dbfaf10d84d983440e2b5a433",
+    "url": "/shopping-cart-ReactAndRedux/static/media/100_2.9b698d6d.jpg"
+  },
+  {
+    "revision": "65be0db0e44455e0e19ace728952ca8e",
+    "url": "/shopping-cart-ReactAndRedux/index.html"
+  }
+];
